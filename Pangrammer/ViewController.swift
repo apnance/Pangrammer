3bb32d454a5306a61ee70f948f06b2182029f8bb
Aaron Nance <apnance@hotmail.com>
2022-01-11 13:52:57 -0700

    Initial Commit

---
+++ Pangrammer/ViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  DePangrammer
+//
+//  Created by Aaron Nance on 1/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - Properties
+    var dictionary = [String]()
+    var pangram = ""
+    
+    
+    // MARK: - Outlets
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var output: UILabel!
+    
+    
+    // MARK: - Actions
+    // MARK: - Overrides
+    
+    
+    // MARK: - Custom Methods
+    override func viewDidLoad() {
+        
+        let hideKeyboard = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard(_:)))
+        
+        view.addGestureRecognizer(hideKeyboard)
+        
+        input.delegate = self
+        
+        super.viewDidLoad()
+        
+    }
+    
+    @objc func hideKeyboard(_ sender: Any) {
+        
+        if input.isFirstResponder {
+            
+            print(type(of: sender))
+            
+            input.resignFirstResponder()
+            
+        }
+        
+    }
+    
+}
+
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if input.text!.count == 7 {
+            
+            input.resignFirstResponder()
+            
+            let word = input.text!
+                    
+            let solution = Pangram.shared.analyze(word)
+            
+            var text = ""
+            for word in solution {
+                text += "\(word)[\(word.count)] "
+            }
+            
+            output.text = text
+            
+            return true
+            
+        } else {
+            
+            output.text = "Invalid Input\nRequired 7 Letter Pangram"
+            return false
+            
+        }
+        
+    }
+    
+}
