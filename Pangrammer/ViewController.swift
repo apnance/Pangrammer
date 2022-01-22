7bc9b2745757433184bee8170a5f3f14014ce091
Aaron Nance <apnance@hotmail.com>
2022-01-21 20:49:03 -0700

    Feature: colorize text via attributed text

--- Pangrammer/ViewController.swift
+++ Pangrammer/ViewController.swift
@@ -16,11 +16,7 @@ class ViewController: UIViewController {
     
     // MARK: - Outlets
     @IBOutlet weak var input: UITextField!
-    @IBOutlet weak var output: UILabel!
-    
-    
-    // MARK: - Actions
-    // MARK: - Overrides
+    @IBOutlet weak var output: UITextView!
     
     
     // MARK: - Custom Methods
@@ -51,24 +47,115 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UITextFieldDelegate {
+
+    enum TextStyle { case head1, head2, pangram, anagram}
+    
+    func builAttributed(text: String,
+                        style: TextStyle,
+                        base: NSMutableAttributedString) {
+        
+        var atts = [NSAttributedString.Key : Any]()
+        atts[ NSAttributedString.Key.font] =  UIFont(name: "Futura-Medium", size: 11.0)!
+
+        switch style {
+            
+        case .head1:
+            
+            atts[ NSAttributedString.Key.font] =  UIFont(name: "Futura-Medium", size: 30.0)!
+            atts[NSAttributedString.Key.foregroundColor] = UIColor.white
+            
+            
+        case .head2:
+            atts[ NSAttributedString.Key.font] =  UIFont(name: "Futura-Medium", size: 12.0)!
+            atts[NSAttributedString.Key.foregroundColor] = UIColor.white
+            
+        case .pangram:
+            
+            atts[NSAttributedString.Key.foregroundColor] = UIColor.systemYellow
+            
+        case .anagram:
+            
+            atts[NSAttributedString.Key.foregroundColor] = UIColor.lightGray
+            
+        }
+        
+        let new = NSMutableAttributedString(string: text,
+                                            attributes: atts)
+        
+        base.append(new)
+        
+    }
+    
+    func setTitle(text: String) {
+        
+        let base = NSMutableAttributedString()
+        
+        builAttributed(text: text.uppercased(),
+                       style: .head1,
+                       base: base)
+        
+        let range = NSRange(location: 3, length: 1)
+        
+        base.addAttribute(NSAttributedString.Key.foregroundColor,
+                          value: UIColor.systemYellow,
+                          range: range)
+        
+        self.input.text = ""
+        self.input.attributedText = base
+        
+    }
+    
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
         if input.text!.count == 7 {
-            
-            input.resignFirstResponder()
-            
-            let word = input.text!
+
+            DispatchQueue.main.async {
+
+                let word = self.input.text!
+                
+                self.setTitle(text: word)
+                
+                self.input.resignFirstResponder()
+                
+                let (all, pan) = Solver.shared.analyze(word)
+
+                let myString = NSMutableAttributedString()
+                
+                self.builAttributed(text: "",
+                                    style: .pangram,
+                                    base: myString)
+                
+                if pan.count > 0 {
+                    self.builAttributed(text: "PANGRAMS(\(pan.count)):\n",
+                                        style: .head2,
+                                        base: myString)
+                }
+                                
+                pan.forEach{
                     
-            let solution = Solver.shared.analyze(word)
-            
-            var text = ""
-            for word in solution {
-                text += "\(word)[\(word.count)] "
+                    self.builAttributed(text: "\($0) - \($0.count)\n",
+                                        style: .pangram,
+                                        base: myString)
+                                        
+                }
+                
+                self.builAttributed(text: "\nANAGRAMS(\(all.count)):\n",
+                                    style: .head2,
+                                    base: myString)
+                
+                all.forEach{
+
+                    self.builAttributed(text: "\($0) - \($0.count)\n",
+                                        style: .anagram,
+                                        base: myString)
+
+                }
+                
+                self.output.attributedText = myString
+                
             }
-            
-            output.text = text
-            
+                
             return true
             
         } else {
