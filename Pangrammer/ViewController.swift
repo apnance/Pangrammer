68247f002a5dceca0b356874da42275a64f87027
Aaron Nance <apnance@hotmail.com>
2023-08-02 12:29:57 -0700

    Feature: enable entry of non-unique letters.  This makes it easier to input the pangram source in the case that the user has found a pangram in the original source and simply wants to check for others.

--- Pangrammer/ViewController.swift
+++ Pangrammer/ViewController.swift
@@ -49,7 +49,7 @@ class ViewController: UIViewController {
 
 extension ViewController: UITextFieldDelegate {
     
-    private enum TextStyle { case head1, head2, pangram, pangramBest, anagram}
+    private enum TextStyle { case head1, head2, pangram, pangramBest, anagram, errorHead, error1, error2}
     
     private func builAttributed(text: String,
                                 style: TextStyle,
@@ -59,29 +59,41 @@ extension ViewController: UITextFieldDelegate {
         atts[ NSAttributedString.Key.font] =  UIFont(name: "Futura-Medium", size: 11.0)!
         
         switch style {
-            
-        case .head1:
-            
-            atts[ NSAttributedString.Key.font] =  UIFont(name: "Futura-Medium", size: 30.0)!
-            atts[NSAttributedString.Key.foregroundColor] = UIColor.white
-            
-            
-        case .head2:
-            atts[ NSAttributedString.Key.font] =  UIFont(name: "Futura-Bold", size: 12.0)!
-            atts[NSAttributedString.Key.foregroundColor] = UIColor.white
-            
-        case .pangram:
-            atts[ NSAttributedString.Key.font] =  UIFont(name: "Futura-Bold", size: 11.0)!
-            atts[NSAttributedString.Key.foregroundColor] = UIColor.systemYellow
-            
-        case .pangramBest:
-            atts[ NSAttributedString.Key.font] =  UIFont(name: "Futura-Bold", size: 11.0)!
-            atts[NSAttributedString.Key.foregroundColor] = UIColor.systemRed
-            
-        case .anagram:
-            
-            atts[NSAttributedString.Key.foregroundColor] = UIColor.lightGray
-            
+                
+            case .head1:
+                
+                atts[ NSAttributedString.Key.font] =  UIFont(name: "Futura-Medium", size: 30.0)!
+                atts[NSAttributedString.Key.foregroundColor] = UIColor.white
+                
+                
+            case .head2:
+                atts[ NSAttributedString.Key.font] =  UIFont(name: "Futura-Bold", size: 12.0)!
+                atts[NSAttributedString.Key.foregroundColor] = UIColor.white
+                
+            case .pangram:
+                atts[ NSAttributedString.Key.font] =  UIFont(name: "Futura-Bold", size: 11.0)!
+                atts[NSAttributedString.Key.foregroundColor] = UIColor.systemYellow
+                
+            case .pangramBest:
+                atts[ NSAttributedString.Key.font] =  UIFont(name: "Futura-Bold", size: 11.0)!
+                atts[NSAttributedString.Key.foregroundColor] = UIColor.systemRed
+                
+            case .anagram:
+                
+                atts[NSAttributedString.Key.foregroundColor] = UIColor.lightGray
+                
+            case .errorHead:
+                atts[ NSAttributedString.Key.font] =  UIFont(name: "Futura-Bold", size: 12.0)!
+                atts[NSAttributedString.Key.foregroundColor] = UIColor.systemRed
+                
+            case .error1:
+                atts[ NSAttributedString.Key.font] =  UIFont(name: "Futura-Medium", size: 11.0)!
+                atts[NSAttributedString.Key.foregroundColor] = UIColor.systemYellow
+                
+            case .error2:
+                atts[ NSAttributedString.Key.font] =  UIFont(name: "Futura", size: 11.0)!
+                atts[NSAttributedString.Key.foregroundColor] = UIColor.lightGray
+                
         }
         
         let new = NSMutableAttributedString(string: text,
@@ -93,85 +105,99 @@ extension ViewController: UITextFieldDelegate {
     
     func setTitle(text: String) {
         
-        let base = NSMutableAttributedString()
+        let baseMAS = NSMutableAttributedString()
         
         builAttributed(text: text.uppercased(),
                        style: .head1,
-                       base: base)
+                       base: baseMAS)
         
         let range = NSRange(location: 3, length: 1)
         
-        base.addAttribute(NSAttributedString.Key.foregroundColor,
+        baseMAS.addAttribute(NSAttributedString.Key.foregroundColor,
                           value: UIColor.systemYellow,
                           range: range)
         
-        self.input.text = ""
-        self.input.attributedText = base
+        self.input.text             = ""
+        self.input.attributedText   = baseMAS
         
     }
     
-    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
-        if input.text!.count == 7 {
-
-            DispatchQueue.main.async {
-
-                let word = self.input.text!
-                
-                self.setTitle(text: word)
-                
-                self.input.resignFirstResponder()
-                
-                let (all, pan) = Solver.shared.analyze(word)
-
-                let myString = NSMutableAttributedString()
-                
-                self.builAttributed(text: "",
-                                    style: .pangram,
-                                    base: myString)
-                
-                if pan.count > 0 {
-                    self.builAttributed(text: "PANGRAMS(\(pan.count)):\n",
+        var success = true
+        let input   = String(Array(input.text!).dedupe())
+        let baseMAS = NSMutableAttributedString()
+        
+            switch input.count {
+                    
+                case 7:
+                    
+                    self.input.text = input
+                    
+                    self.setTitle(text: input)
+                    
+                    self.input.resignFirstResponder()
+                    
+                    let (all, pan) = Solver.shared.analyze(input)
+                    
+                    self.builAttributed(text: "",
+                                        style: .pangram,
+                                        base: baseMAS)
+                    
+                    if pan.count > 0 {
+                        self.builAttributed(text: "PANGRAMS(\(pan.count)):\n",
+                                            style: .head2,
+                                            base: baseMAS)
+                    }
+                    
+                    var style: TextStyle = .pangramBest
+                    pan.forEach{
+                        
+                        self.builAttributed(text: "\($0) - \($0.count)\n",
+                                            style: style,
+                                            base: baseMAS)
+                        
+                        style = .pangram
+                        
+                    }
+                    
+                    self.builAttributed(text: "\nANAGRAMS(\(all.count)):\n",
                                         style: .head2,
-                                        base: myString)
-                }
+                                        base: baseMAS)
                     
-                var style: TextStyle = .pangramBest
-                pan.forEach{
+                    all.forEach{
+                        
+                        self.builAttributed(text: "\($0) - \($0.count)\n",
+                                            style: .anagram,
+                                            base: baseMAS)
+                        
+                    }
                     
-                    self.builAttributed(text: "\($0) - \($0.count)\n",
-                                        style: style,
-                                        base: myString)
+                    success = true
                     
-                    style = .pangram
+                default:
                     
-                }
-                
-                self.builAttributed(text: "\nANAGRAMS(\(all.count)):\n",
-                                    style: .head2,
-                                    base: myString)
-                
-                all.forEach{
-
-                    self.builAttributed(text: "\($0) - \($0.count)\n",
-                                        style: .anagram,
-                                        base: myString)
-
-                }
-                
-                self.output.attributedText = myString
-                
+                    let text = input.count > 7 ? "many" : "few"
+                    
+                    self.builAttributed(text: "Invalid Input:\n",
+                                        style: .errorHead,
+                                        base: baseMAS)
+                    
+                    self.builAttributed(text: "Too \(text) characters(\(input.count)) - ",
+                                        style: .error1,
+                                        base: baseMAS)
+                    
+                    self.builAttributed(text: "Pangram input must be 7 unique characters.",
+                                        style: .error2,
+                                        base: baseMAS)
+                    
+                    success = false
+                    
             }
-                
-            return true
+        
+        DispatchQueue.main.async { self.output.attributedText = baseMAS }
             
-        } else {
-            
-            output.text = "Invalid Input\nRequired 7 Letter Pangram"
-            return false
-            
-        }
+        return success /*EXIT*/
         
     }
     
